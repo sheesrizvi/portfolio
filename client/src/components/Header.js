aae54567733a262e232fee3d8432615ddf5100e2
Tanmay <you@example.com>
2021-10-23 18:42:48 +0530

    frontend and navbar setup

---
+++ client/src/components/Header.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import Home from '../pages/Home'
+import About from '../pages/About'
+import Services from '../pages/Services'
+import Contact from '../pages/Contact'
+
+const Header = () => {
+  return (
+    <Router>
+      <nav>
+        <div className='logo'>
+          <h1>Startup</h1>
+        </div>
+        <div className='links'>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+            <li>
+              <Link to='/services'>Services</Link>
+            </li>
+            <li>
+              <Link to='/contact'>Contacts</Link>
+            </li>
+          </ul>
+          <Switch>
+            <Route path='/about'>
+              <About />
+            </Route>
+            <Route path='/services'>
+              <Services />
+            </Route>
+            <Route path='/'>
+              <Home />
+            </Route>
+            <Route path='/contact'>
+              <Contact />
+            </Route>
+          </Switch>
+        </div>
+        <div className='cart'>
+          {' '}
+          <Link to='/cart'>🛒</Link>
+        </div>
+      </nav>
+    </Router>
+  )
+}
+
+export default Header
